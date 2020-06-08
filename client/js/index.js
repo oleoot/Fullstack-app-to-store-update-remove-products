63d55327dda44be719df0ac69007b399c5377131
oleoot <oleoot28@gmail.com>
2020-06-08 18:29:33 +0300

    Getting info from server to client

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+
+
+const API_URL = 'http://localhost:3000/';
+
+
+fetch(API_URL)
+    .then(res => res.json())
+    .then(result => {
+        console.log(result)
+    })
--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express()
+const cors = require('cors')
 
-
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello world'
